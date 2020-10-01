e608859810c293bb15c8adb84d216f5f85313d43
JackTheWanted <58087382+JackTheWanted@users.noreply.github.com>
2020-10-01 18:39:38 -0400

    ran into massive brick wall in hangman

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,427 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnA = new System.Windows.Forms.Button();
+            this.btnB = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btnD = new System.Windows.Forms.Button();
+            this.btnE = new System.Windows.Forms.Button();
+            this.btnF = new System.Windows.Forms.Button();
+            this.btnG = new System.Windows.Forms.Button();
+            this.btnH = new System.Windows.Forms.Button();
+            this.btnK = new System.Windows.Forms.Button();
+            this.btnL = new System.Windows.Forms.Button();
+            this.btnM = new System.Windows.Forms.Button();
+            this.btnN = new System.Windows.Forms.Button();
+            this.btnO = new System.Windows.Forms.Button();
+            this.btnP = new System.Windows.Forms.Button();
+            this.btnQ = new System.Windows.Forms.Button();
+            this.btnR = new System.Windows.Forms.Button();
+            this.btnS = new System.Windows.Forms.Button();
+            this.btnT = new System.Windows.Forms.Button();
+            this.btnU = new System.Windows.Forms.Button();
+            this.btnI = new System.Windows.Forms.Button();
+            this.btnJ = new System.Windows.Forms.Button();
+            this.btnV = new System.Windows.Forms.Button();
+            this.btnW = new System.Windows.Forms.Button();
+            this.btnX = new System.Windows.Forms.Button();
+            this.btnZ = new System.Windows.Forms.Button();
+            this.btnY = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblWord = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(236, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hangman Lite";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(79, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(653, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Click on one of the letters to make a guess, three wrong guesses and you\'re out.";
+            // 
+            // btnA
+            // 
+            this.btnA.Location = new System.Drawing.Point(48, 407);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(21, 23);
+            this.btnA.TabIndex = 3;
+            this.btnA.Text = "A";
+            this.btnA.UseVisualStyleBackColor = true;
+            this.btnA.Click += new System.EventHandler(this.btnA_Click);
+            // 
+            // btnB
+            // 
+            this.btnB.Location = new System.Drawing.Point(75, 407);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(21, 23);
+            this.btnB.TabIndex = 4;
+            this.btnB.Text = "B";
+            this.btnB.UseVisualStyleBackColor = true;
+            this.btnB.Click += new System.EventHandler(this.btnB_Click);
+            // 
+            // btnC
+            // 
+            this.btnC.Location = new System.Drawing.Point(102, 407);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(21, 23);
+            this.btnC.TabIndex = 5;
+            this.btnC.Text = "C";
+            this.btnC.UseVisualStyleBackColor = true;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // btnD
+            // 
+            this.btnD.Location = new System.Drawing.Point(129, 407);
+            this.btnD.Name = "btnD";
+            this.btnD.Size = new System.Drawing.Size(21, 23);
+            this.btnD.TabIndex = 6;
+            this.btnD.Text = "D";
+            this.btnD.UseVisualStyleBackColor = true;
+            this.btnD.Click += new System.EventHandler(this.btnD_Click);
+            // 
+            // btnE
+            // 
+            this.btnE.Location = new System.Drawing.Point(156, 407);
+            this.btnE.Name = "btnE";
+            this.btnE.Size = new System.Drawing.Size(21, 23);
+            this.btnE.TabIndex = 7;
+            this.btnE.Text = "E";
+            this.btnE.UseVisualStyleBackColor = true;
+            this.btnE.Click += new System.EventHandler(this.btnE_Click);
+            // 
+            // btnF
+            // 
+            this.btnF.Location = new System.Drawing.Point(183, 407);
+            this.btnF.Name = "btnF";
+            this.btnF.Size = new System.Drawing.Size(21, 23);
+            this.btnF.TabIndex = 8;
+            this.btnF.Text = "F";
+            this.btnF.UseVisualStyleBackColor = true;
+            // 
+            // btnG
+            // 
+            this.btnG.Location = new System.Drawing.Point(210, 407);
+            this.btnG.Name = "btnG";
+            this.btnG.Size = new System.Drawing.Size(21, 23);
+            this.btnG.TabIndex = 9;
+            this.btnG.Text = "G";
+            this.btnG.UseVisualStyleBackColor = true;
+            this.btnG.Click += new System.EventHandler(this.btnG_Click);
+            // 
+            // btnH
+            // 
+            this.btnH.Location = new System.Drawing.Point(237, 407);
+            this.btnH.Name = "btnH";
+            this.btnH.Size = new System.Drawing.Size(21, 23);
+            this.btnH.TabIndex = 10;
+            this.btnH.Text = "H";
+            this.btnH.UseVisualStyleBackColor = true;
+            this.btnH.Click += new System.EventHandler(this.btnH_Click);
+            // 
+            // btnK
+            // 
+            this.btnK.Location = new System.Drawing.Point(318, 407);
+            this.btnK.Name = "btnK";
+            this.btnK.Size = new System.Drawing.Size(20, 23);
+            this.btnK.TabIndex = 12;
+            this.btnK.Text = "K";
+            this.btnK.UseVisualStyleBackColor = true;
+            this.btnK.Click += new System.EventHandler(this.btnK_Click);
+            // 
+            // btnL
+            // 
+            this.btnL.Location = new System.Drawing.Point(344, 407);
+            this.btnL.Name = "btnL";
+            this.btnL.Size = new System.Drawing.Size(21, 23);
+            this.btnL.TabIndex = 14;
+            this.btnL.Text = "L";
+            this.btnL.UseVisualStyleBackColor = true;
+            this.btnL.Click += new System.EventHandler(this.btnL_Click);
+            // 
+            // btnM
+            // 
+            this.btnM.Location = new System.Drawing.Point(371, 407);
+            this.btnM.Name = "btnM";
+            this.btnM.Size = new System.Drawing.Size(23, 23);
+            this.btnM.TabIndex = 15;
+            this.btnM.Text = "M";
+            this.btnM.UseVisualStyleBackColor = true;
+            this.btnM.Click += new System.EventHandler(this.btnM_Click);
+            // 
+            // btnN
+            // 
+            this.btnN.Location = new System.Drawing.Point(400, 407);
+            this.btnN.Name = "btnN";
+            this.btnN.Size = new System.Drawing.Size(24, 23);
+            this.btnN.TabIndex = 16;
+            this.btnN.Text = "N";
+            this.btnN.UseVisualStyleBackColor = true;
+            this.btnN.Click += new System.EventHandler(this.btnN_Click);
+            // 
+            // btnO
+            // 
+            this.btnO.Location = new System.Drawing.Point(430, 407);
+            this.btnO.Name = "btnO";
+            this.btnO.Size = new System.Drawing.Size(21, 23);
+            this.btnO.TabIndex = 17;
+            this.btnO.Text = "O";
+            this.btnO.UseVisualStyleBackColor = true;
+            this.btnO.Click += new System.EventHandler(this.btnO_Click);
+            // 
+            // btnP
+            // 
+            this.btnP.Location = new System.Drawing.Point(457, 407);
+            this.btnP.Name = "btnP";
+            this.btnP.Size = new System.Drawing.Size(21, 23);
+            this.btnP.TabIndex = 18;
+            this.btnP.Text = "P";
+            this.btnP.UseVisualStyleBackColor = true;
+            this.btnP.Click += new System.EventHandler(this.btnP_Click);
+            // 
+            // btnQ
+            // 
+            this.btnQ.Location = new System.Drawing.Point(484, 407);
+            this.btnQ.Name = "btnQ";
+            this.btnQ.Size = new System.Drawing.Size(21, 23);
+            this.btnQ.TabIndex = 19;
+            this.btnQ.Text = "Q";
+            this.btnQ.UseVisualStyleBackColor = true;
+            this.btnQ.Click += new System.EventHandler(this.btnQ_Click);
+            // 
+            // btnR
+            // 
+            this.btnR.Location = new System.Drawing.Point(511, 407);
+            this.btnR.Name = "btnR";
+            this.btnR.Size = new System.Drawing.Size(21, 23);
+            this.btnR.TabIndex = 20;
+            this.btnR.Text = "R";
+            this.btnR.UseVisualStyleBackColor = true;
+            this.btnR.Click += new System.EventHandler(this.btnR_Click);
+            // 
+            // btnS
+            // 
+            this.btnS.Location = new System.Drawing.Point(538, 407);
+            this.btnS.Name = "btnS";
+            this.btnS.Size = new System.Drawing.Size(21, 23);
+            this.btnS.TabIndex = 21;
+            this.btnS.Text = "S";
+            this.btnS.UseVisualStyleBackColor = true;
+            this.btnS.Click += new System.EventHandler(this.btnS_Click);
+            // 
+            // btnT
+            // 
+            this.btnT.Location = new System.Drawing.Point(565, 407);
+            this.btnT.Name = "btnT";
+            this.btnT.Size = new System.Drawing.Size(21, 23);
+            this.btnT.TabIndex = 22;
+            this.btnT.Text = "T";
+            this.btnT.UseVisualStyleBackColor = true;
+            this.btnT.Click += new System.EventHandler(this.btnT_Click);
+            // 
+            // btnU
+            // 
+            this.btnU.Location = new System.Drawing.Point(592, 407);
+            this.btnU.Name = "btnU";
+            this.btnU.Size = new System.Drawing.Size(21, 23);
+            this.btnU.TabIndex = 23;
+            this.btnU.Text = "U";
+            this.btnU.UseVisualStyleBackColor = true;
+            // 
+            // btnI
+            // 
+            this.btnI.Location = new System.Drawing.Point(264, 407);
+            this.btnI.Name = "btnI";
+            this.btnI.Size = new System.Drawing.Size(21, 23);
+            this.btnI.TabIndex = 24;
+            this.btnI.Text = "I";
+            this.btnI.UseVisualStyleBackColor = true;
+            this.btnI.Click += new System.EventHandler(this.btnI_Click);
+            // 
+            // btnJ
+            // 
+            this.btnJ.Location = new System.Drawing.Point(291, 407);
+            this.btnJ.Name = "btnJ";
+            this.btnJ.Size = new System.Drawing.Size(21, 23);
+            this.btnJ.TabIndex = 25;
+            this.btnJ.Text = "J";
+            this.btnJ.UseVisualStyleBackColor = true;
+            this.btnJ.Click += new System.EventHandler(this.btnJ_Click);
+            // 
+            // btnV
+            // 
+            this.btnV.Location = new System.Drawing.Point(619, 407);
+            this.btnV.Name = "btnV";
+            this.btnV.Size = new System.Drawing.Size(21, 23);
+            this.btnV.TabIndex = 26;
+            this.btnV.Text = "V";
+            this.btnV.UseVisualStyleBackColor = true;
+            this.btnV.Click += new System.EventHandler(this.btnV_Click);
+            // 
+            // btnW
+            // 
+            this.btnW.Location = new System.Drawing.Point(646, 407);
+            this.btnW.Name = "btnW";
+            this.btnW.Size = new System.Drawing.Size(21, 23);
+            this.btnW.TabIndex = 27;
+            this.btnW.Text = "W";
+            this.btnW.UseVisualStyleBackColor = true;
+            this.btnW.Click += new System.EventHandler(this.btnW_Click);
+            // 
+            // btnX
+            // 
+            this.btnX.Location = new System.Drawing.Point(673, 407);
+            this.btnX.Name = "btnX";
+            this.btnX.Size = new System.Drawing.Size(21, 23);
+            this.btnX.TabIndex = 28;
+            this.btnX.Text = "X";
+            this.btnX.UseVisualStyleBackColor = true;
+            this.btnX.Click += new System.EventHandler(this.btnX_Click);
+            // 
+            // btnZ
+            // 
+            this.btnZ.Location = new System.Drawing.Point(727, 407);
+            this.btnZ.Name = "btnZ";
+            this.btnZ.Size = new System.Drawing.Size(21, 23);
+            this.btnZ.TabIndex = 29;
+            this.btnZ.Text = "Z";
+            this.btnZ.UseVisualStyleBackColor = true;
+            this.btnZ.Click += new System.EventHandler(this.btnZ_Click);
+            // 
+            // btnY
+            // 
+            this.btnY.Location = new System.Drawing.Point(700, 407);
+            this.btnY.Name = "btnY";
+            this.btnY.Size = new System.Drawing.Size(21, 23);
+            this.btnY.TabIndex = 30;
+            this.btnY.Text = "Y";
+            this.btnY.UseVisualStyleBackColor = true;
+            this.btnY.Click += new System.EventHandler(this.btnY_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Hangman_Lite__Part_8_Project_.Properties.Resources.stickperson;
+            this.pictureBox2.Location = new System.Drawing.Point(131, 141);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(234, 223);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 32;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Hangman_Lite__Part_8_Project_.Properties.Resources.HangPole;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 84);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(804, 317);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 31;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblWord
+            // 
+            this.lblWord.AutoSize = true;
+            this.lblWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 45.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWord.Location = new System.Drawing.Point(371, 316);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(414, 70);
+            this.lblWord.TabIndex = 33;
+            this.lblWord.Text = "_ _ _ _ _ _ _ _";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(795, 442);
+            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnY);
+            this.Controls.Add(this.btnZ);
+            this.Controls.Add(this.btnX);
+            this.Controls.Add(this.btnW);
+            this.Controls.Add(this.btnV);
+            this.Controls.Add(this.btnJ);
+            this.Controls.Add(this.btnI);
+            this.Controls.Add(this.btnU);
+            this.Controls.Add(this.btnT);
+            this.Controls.Add(this.btnS);
+            this.Controls.Add(this.btnR);
+            this.Controls.Add(this.btnQ);
+            this.Controls.Add(this.btnP);
+            this.Controls.Add(this.btnO);
+            this.Controls.Add(this.btnN);
+            this.Controls.Add(this.btnM);
+            this.Controls.Add(this.btnL);
+            this.Controls.Add(this.btnK);
+            this.Controls.Add(this.btnH);
+            this.Controls.Add(this.btnG);
+            this.Controls.Add(this.btnF);
+            this.Controls.Add(this.btnE);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.btnA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Hangman";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btnD;
+        private System.Windows.Forms.Button btnE;
+        private System.Windows.Forms.Button btnF;
+        private System.Windows.Forms.Button btnG;
+        private System.Windows.Forms.Button btnH;
+        private System.Windows.Forms.Button btnK;
+        private System.Windows.Forms.Button btnL;
+        private System.Windows.Forms.Button btnM;
+        private System.Windows.Forms.Button btnN;
+        private System.Windows.Forms.Button btnO;
+        private System.Windows.Forms.Button btnP;
+        private System.Windows.Forms.Button btnQ;
+        private System.Windows.Forms.Button btnR;
+        private System.Windows.Forms.Button btnS;
+        private System.Windows.Forms.Button btnT;
+        private System.Windows.Forms.Button btnU;
+        private System.Windows.Forms.Button btnI;
+        private System.Windows.Forms.Button btnJ;
+        private System.Windows.Forms.Button btnV;
+        private System.Windows.Forms.Button btnW;
+        private System.Windows.Forms.Button btnX;
+        private System.Windows.Forms.Button btnZ;
+        private System.Windows.Forms.Button btnY;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lblWord;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,146 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+List<string> wordList = new List<string>() { "COMPUTER", "AMBUSHES", "MOUNTAIN", "SURPRISE", "OBTAINED", "FLAVOURS", "TERMINAL", "UNIVERSE", "WORKSHOP", "REPUBLIC" };
+Random generator = new Random();
 namespace Hangman_Lite__Part_8_Project_
 {
+
     public partial class Form1 : Form
     {
+        
+        string word = "_ _ _ _ _ _ _ _";
+        string secretWord = wordList[generator.Next(10)];
+
         public Form1()
         {
             InitializeComponent();
+            pictureBox1.Controls.Add(pictureBox2);
+            pictureBox2.Location = new Point(131, 58);
+            pictureBox2.BackColor = Color.Transparent;
+            
+            
+        }
+        
+
+        private void btnA_Click(object sender, EventArgs e)
+        {
+                     
+        }
+
+        private void btnB_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnC_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnD_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnE_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnG_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnH_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnI_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnJ_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnK_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnL_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnM_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnN_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnO_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnP_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnQ_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnR_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnS_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnT_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnV_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnW_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnX_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnY_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnZ_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
